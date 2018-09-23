3332854bf864c597eac87445046d6c7caf1c5a38
Max <tolive@ukr,net>
2018-09-24 00:18:29 +0300

    leson 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  users = [
+    {name: 'WFM 1'},
+    {name: 'WFM 2'},
+    {name: 'WFM 3'},
+    {name: 'WFM 4'},
+    {name: 'WFM 5'},
+    {name: 'WFM 6'},
+    {name: 'WFM 7'},
+    {name: 'WFM 8'}
+  ]
 }
